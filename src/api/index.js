3f3ec16436c48d4559e6f94460770d76d11976b9
anushkax2311 <146274154+anushkax2311@users.noreply.github.com>
2026-05-23 11:31:20 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api', headers: { 'Content-Type': 'application/json' } })
+const api = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL || '/api' 
+})
 
 // Event Types
 export const getEventTypes       = ()        => api.get('/event-types')
