f5afec0434c6365748d1f0ed40d5c8b3019f20d4
anushkax2311 <146274154+anushkax2311@users.noreply.github.com>
2026-05-23 13:29:23 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
   "http://localhost:5173",
-  "https://your-app.vercel.app"
+  "calendly-clone-pylw.vercel.app"
 ],
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
